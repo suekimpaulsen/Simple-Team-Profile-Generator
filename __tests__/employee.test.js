44d6e552b9120da90e5af0db06b7fc964f903937
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-06-22 21:24:30 -0500

    employee added, test passed

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,1 +1,33 @@
 const Employee = require('../lib/employee')
+
+test('creates an employee object', () => {
+    const employee = new Employee('employee name', 'employee id', 'employee email')
+    
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(String));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test('check if the getName function worked', () => {
+    const employee = new Employee('employee name', 'employee id', 'employee email')
+
+    expect(employee.getName()).toEqual(employee.name);
+});
+
+test('check if the getId function worked', () => {
+    const employee = new Employee('employee name', 'employee id', 'employee email')
+
+    expect(employee.getId()).toEqual(employee.id);
+});
+test('check if the getEmail function worked', () => {
+    const employee = new Employee('employee name', 'employee id', 'employee email')
+
+    expect(employee.getEmail()).toEqual(employee.email);
+});
+
+test('check if the correct role is returned', () => {
+    const employee = new Employee('employee name', 'employee id', 'employee email')
+
+    expect(employee.getRole()).toBe('Employee');
+});
+
