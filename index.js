27b1f29762fa25608ef88eb9e80d9980416f3eec
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-06-22 21:41:01 -0500

    notes added for index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateSite = require('./src/generate-site');
+
+//manager question array
+
+//engineer question arry
+
+//intern question array
+
+//init function
+function init() {
+    inquirer
+    .prompt()
+    .then()
+}
+// function call to initalize app
+init();
